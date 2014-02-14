4596dc766701dc6f59503604f8faf28fa50a16a7
rmurray <ryanm@zoovy.com>
2014-02-13 16:39:21 -0800

    Homepage side banner content to search for new arrival
    Made search version of template so lists, or search results could easily
    be used here.

--- app-quickstart.js
+++ app-quickstart.js
@@ -30,6 +30,7 @@ var myRIA = function() {
 //custom app specific templates		
 			'quickCartTemplate',
 			'sideBannerProductListTemplate',
+			'sideBannerProductSearchTemplate',
 //the list of templates that are commonly edited (same order as they appear in appTemplates
 			'homepageTemplate',	'categoryTemplate',
 			'categoryListTemplate',
@@ -3136,7 +3137,7 @@ else	{
 			createTemplateFunctions : function()	{
 
 				app.ext.myRIA.template = {};
-				var pageTemplates = new Array('quickCartTemplate','sideBannerProductListTemplate','categoryTemplate','productTemplate','companyTemplate','customerTemplate','homepageTemplate','searchTemplate','cartTemplate','checkoutTemplate','pageNotFoundTemplate');
+				var pageTemplates = new Array('quickCartTemplate','sideBannerProductSearchTemplate','sideBannerProductListTemplate','categoryTemplate','productTemplate','companyTemplate','customerTemplate','homepageTemplate','searchTemplate','cartTemplate','checkoutTemplate','pageNotFoundTemplate');
 				var L = pageTemplates.length;
 				for(var i = 0; i < L; i += 1)	{
 					app.ext.myRIA.template[pageTemplates[i]] = {"onCompletes":[],"onInits":[],"onDeparts":[]};
